31409e44d73073cbab12d27689fd9f8048d7ab9b
ManyTheFish <many@meilisearch.com>
2022-05-03 16:18:51 +0200

    Force tabs in rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -39,107 +39,107 @@ use fst::raw::{Fst, Output};
 use once_cell::sync::Lazy;
 
 static T2S: Lazy<HashMap<String, String>> =
-    Lazy::new(|| deserialize_from(&include_bytes!("../data/t2s.profile")[..]).unwrap());
+	Lazy::new(|| deserialize_from(&include_bytes!("../data/t2s.profile")[..]).unwrap());
 static S2T: Lazy<HashMap<String, String>> =
-    Lazy::new(|| deserialize_from(&include_bytes!("../data/s2t.profile")[..]).unwrap());
+	Lazy::new(|| deserialize_from(&include_bytes!("../data/s2t.profile")[..]).unwrap());
 
 // create an fst containing all the keys
 static T2S_FST: Lazy<Fst<Vec<u8>>> = Lazy::new(|| {
-    let mut keys: Vec<_> = T2S.keys().collect();
-    keys.sort_unstable();
-    Fst::from_iter_set(keys).unwrap()
+	let mut keys: Vec<_> = T2S.keys().collect();
+	keys.sort_unstable();
+	Fst::from_iter_set(keys).unwrap()
 });
 
 // create an fst containing all the keys
 static S2T_FST: Lazy<Fst<Vec<u8>>> = Lazy::new(|| {
-    let mut keys: Vec<_> = S2T.keys().collect();
-    keys.sort_unstable();
-    Fst::from_iter_set(keys).unwrap()
+	let mut keys: Vec<_> = S2T.keys().collect();
+	keys.sort_unstable();
+	Fst::from_iter_set(keys).unwrap()
 });
 
 fn is_script(
-    raw: &str,
-    mapping: &HashMap<String, String>,
-    backup: &HashMap<String, String>,
+	raw: &str,
+	mapping: &HashMap<String, String>,
+	backup: &HashMap<String, String>,
 ) -> bool {
-    let mut buffer = [0; 4];
-    for character in raw.chars() {
-        let c = character.encode_utf8(&mut buffer);
-        if !mapping.contains_key(c) {
-            if backup.contains_key(c) {
-                return false;
-            }
-        }
-    }
+	let mut buffer = [0; 4];
+	for character in raw.chars() {
+		let c = character.encode_utf8(&mut buffer);
+		if !mapping.contains_key(c) {
+			if backup.contains_key(c) {
+				return false;
+			}
+		}
+	}
 
-    return true;
+	return true;
 }
 
 pub fn is_traditional(raw: &str) -> bool {
-    is_script(raw, &T2S, &S2T)
+	is_script(raw, &T2S, &S2T)
 }
 
 pub fn is_simplified(raw: &str) -> bool {
-    is_script(raw, &S2T, &T2S)
+	is_script(raw, &S2T, &T2S)
 }
 
 fn convert_script<'a>(
-    raw: &'a str,
-    mapping: &HashMap<String, String>,
-    fst: &Fst<Vec<u8>>,
+	raw: &'a str,
+	mapping: &HashMap<String, String>,
+	fst: &Fst<Vec<u8>>,
 ) -> Cow<'a, str> {
-    let mut converted_characters: Option<String> = None;
-    let mut skip_bytes = 0;
+	let mut converted_characters: Option<String> = None;
+	let mut skip_bytes = 0;
 
-    while skip_bytes < raw.len() {
-        let tailstr = &raw[skip_bytes..];
+	while skip_bytes < raw.len() {
+		let tailstr = &raw[skip_bytes..];
 
-        match find_longest_prefix(fst, tailstr.as_bytes()) {
-            Some((_, length)) => {
-                let tailstr = &tailstr[..length];
-                let mapped = mapping.get(tailstr).unwrap();
+		match find_longest_prefix(fst, tailstr.as_bytes()) {
+			Some((_, length)) => {
+				let tailstr = &tailstr[..length];
+				let mapped = mapping.get(tailstr).unwrap();
 
-                converted_characters = match converted_characters.take() {
-                    Some(mut converted_characters) => {
-                        converted_characters.push_str(mapped);
-                        Some(converted_characters)
-                    }
-                    None => {
-                        if tailstr != mapped {
-                            let mut converted_characters = String::with_capacity(raw.len());
-                            converted_characters.push_str(&raw[..skip_bytes]);
-                            converted_characters.push_str(mapped);
-                            Some(converted_characters)
-                        } else {
-                            None
-                        }
-                    }
-                };
-                skip_bytes += tailstr.len();
-            }
-            None => {
-                let first = tailstr.chars().next().unwrap();
-                if let Some(converted_characters) = converted_characters.as_mut() {
-                    converted_characters.push(first);
-                }
+				converted_characters = match converted_characters.take() {
+					Some(mut converted_characters) => {
+						converted_characters.push_str(mapped);
+						Some(converted_characters)
+					}
+					None => {
+						if tailstr != mapped {
+							let mut converted_characters = String::with_capacity(raw.len());
+							converted_characters.push_str(&raw[..skip_bytes]);
+							converted_characters.push_str(mapped);
+							Some(converted_characters)
+						} else {
+							None
+						}
+					}
+				};
+				skip_bytes += tailstr.len();
+			}
+			None => {
+				let first = tailstr.chars().next().unwrap();
+				if let Some(converted_characters) = converted_characters.as_mut() {
+					converted_characters.push(first);
+				}
 
-                skip_bytes += first.len_utf8();
-            }
-        }
-    }
+				skip_bytes += first.len_utf8();
+			}
+		}
+	}
 
-    match converted_characters {
-        Some(s) => Cow::Owned(s),
-        None => Cow::Borrowed(raw),
-    }
+	match converted_characters {
+		Some(s) => Cow::Owned(s),
+		None => Cow::Borrowed(raw),
+	}
 }
 
 pub fn traditional_to_simplified(raw: &str) -> Cow<str> {
-    convert_script(raw, &T2S, &T2S_FST)
+	convert_script(raw, &T2S, &T2S_FST)
 }
 
 pub fn simplified_to_traditional(raw: &str) -> Cow<str> {
-    convert_script(raw, &S2T, &S2T_FST)
+	convert_script(raw, &S2T, &S2T_FST)
 }
 
 /// Thanks to @llogiq for this function
@@ -154,127 +154,127 @@ pub fn simplified_to_traditional(raw: &str) -> Cow<str> {
 /// This can be used to e.g. build tokenizing functions.
 #[inline]
 fn find_longest_prefix(fst: &Fst<Vec<u8>>, value: &[u8]) -> Option<(u64, usize)> {
-    let mut node = fst.root();
-    let mut out = Output::zero();
-    let mut last_match = None;
-    for (i, &b) in value.iter().enumerate() {
-        if let Some(trans_index) = node.find_input(b) {
-            let t = node.transition(trans_index);
-            node = fst.node(t.addr);
-            out = out.cat(t.out);
-            if node.is_final() {
-                last_match = Some((out.cat(node.final_output()).value(), i + 1));
-            }
-        } else {
-            return last_match;
-        }
-    }
-    last_match
+	let mut node = fst.root();
+	let mut out = Output::zero();
+	let mut last_match = None;
+	for (i, &b) in value.iter().enumerate() {
+		if let Some(trans_index) = node.find_input(b) {
+			let t = node.transition(trans_index);
+			node = fst.node(t.addr);
+			out = out.cat(t.out);
+			if node.is_final() {
+				last_match = Some((out.cat(node.final_output()).value(), i + 1));
+			}
+		} else {
+			return last_match;
+		}
+	}
+	last_match
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn test_is_traditional() {
-        let simplified = "欧洲";
-        let traditional = "歐洲";
-        assert_eq!(true, is_traditional(traditional));
-        assert_eq!(false, is_traditional(simplified));
-    }
+	#[test]
+	fn test_is_traditional() {
+		let simplified = "欧洲";
+		let traditional = "歐洲";
+		assert_eq!(true, is_traditional(traditional));
+		assert_eq!(false, is_traditional(simplified));
+	}
 
-    #[test]
-    fn test_is_simplified() {
-        let simplified = "欧洲";
-        let traditional = "歐洲";
-        assert_eq!(true, is_simplified(simplified));
-        assert_eq!(false, is_simplified(traditional));
-    }
+	#[test]
+	fn test_is_simplified() {
+		let simplified = "欧洲";
+		let traditional = "歐洲";
+		assert_eq!(true, is_simplified(simplified));
+		assert_eq!(false, is_simplified(traditional));
+	}
 
-    #[test]
-    fn test_traditional_to_simplified() {
-        let simplified = "欧洲";
-        let traditional = "歐洲";
-        assert_eq!(simplified, traditional_to_simplified(traditional));
+	#[test]
+	fn test_traditional_to_simplified() {
+		let simplified = "欧洲";
+		let traditional = "歐洲";
+		assert_eq!(simplified, traditional_to_simplified(traditional));
 
-        let traditional = "人人生而自由﹐在尊嚴和權利上一律平等。他們賦有理性和良心﹐並應以兄弟關係的精神互相對待。";
-        let simplified = "人人生而自由﹐在尊严和权利上一律平等。他们赋有理性和良心﹐并应以兄弟关系的精神互相对待。";
-        assert_eq!(simplified, traditional_to_simplified(traditional));
-    }
+		let traditional = "人人生而自由﹐在尊嚴和權利上一律平等。他們賦有理性和良心﹐並應以兄弟關係的精神互相對待。";
+		let simplified = "人人生而自由﹐在尊严和权利上一律平等。他们赋有理性和良心﹐并应以兄弟关系的精神互相对待。";
+		assert_eq!(simplified, traditional_to_simplified(traditional));
+	}
 
-    #[test]
-    fn test_simplified_to_traditional() {
-        let simplified = "欧洲";
-        let traditional = "歐洲";
-        assert_eq!(traditional, simplified_to_traditional(simplified));
+	#[test]
+	fn test_simplified_to_traditional() {
+		let simplified = "欧洲";
+		let traditional = "歐洲";
+		assert_eq!(traditional, simplified_to_traditional(simplified));
 
-        let traditional = "人人生而自由﹐在尊嚴咊權利上一律平等。他們賦有理性咊良心﹐並應㕥兄弟關係的精神互相對待。";
-        let simplified = "人人生而自由﹐在尊严和权利上一律平等。他们赋有理性和良心﹐并应以兄弟关系的精神互相对待。";
-        assert_eq!(traditional, simplified_to_traditional(simplified));
-    }
+		let traditional = "人人生而自由﹐在尊嚴咊權利上一律平等。他們賦有理性咊良心﹐並應㕥兄弟關係的精神互相對待。";
+		let simplified = "人人生而自由﹐在尊严和权利上一律平等。他们赋有理性和良心﹐并应以兄弟关系的精神互相对待。";
+		assert_eq!(traditional, simplified_to_traditional(simplified));
+	}
 }
 
 #[cfg(all(feature = "bench", test))]
 mod benches {
-    extern crate test;
-    use test::Bencher;
+	extern crate test;
+	use test::Bencher;
 
-    use super::*;
+	use super::*;
 
-    #[bench]
-    #[cfg(feature = "bench")]
-    fn bench_traditional_to_simplified(b: &mut Bencher) {
-        let traditional = "人人生而自由﹐在尊嚴和權利上一律平等。他們賦有理性和良心﹐並應以兄弟關係的精神互相對待。";
-        b.iter(|| traditional_to_simplified(traditional));
-    }
+	#[bench]
+	#[cfg(feature = "bench")]
+	fn bench_traditional_to_simplified(b: &mut Bencher) {
+		let traditional = "人人生而自由﹐在尊嚴和權利上一律平等。他們賦有理性和良心﹐並應以兄弟關係的精神互相對待。";
+		b.iter(|| traditional_to_simplified(traditional));
+	}
 
-    #[bench]
-    #[cfg(feature = "bench")]
-    fn bench_simplified_to_traditional(b: &mut Bencher) {
-        let simplified = "人人生而自由﹐在尊严和权利上一律平等。他们赋有理性和良心﹐并应以兄弟关系的精神互相对待。";
-        b.iter(|| simplified_to_traditional(simplified));
-    }
+	#[bench]
+	#[cfg(feature = "bench")]
+	fn bench_simplified_to_traditional(b: &mut Bencher) {
+		let simplified = "人人生而自由﹐在尊严和权利上一律平等。他们赋有理性和良心﹐并应以兄弟关系的精神互相对待。";
+		b.iter(|| simplified_to_traditional(simplified));
+	}
 
-    #[bench]
-    #[cfg(feature = "bench")]
-    fn bench_traditional_to_traditional(b: &mut Bencher) {
-        let traditional = "人人生而自由﹐在尊嚴咊權利上一律平等。他們賦有理性咊良心﹐並應㕥兄弟關係的精神互相對待。";
-        b.iter(|| simplified_to_traditional(traditional));
-    }
+	#[bench]
+	#[cfg(feature = "bench")]
+	fn bench_traditional_to_traditional(b: &mut Bencher) {
+		let traditional = "人人生而自由﹐在尊嚴咊權利上一律平等。他們賦有理性咊良心﹐並應㕥兄弟關係的精神互相對待。";
+		b.iter(|| simplified_to_traditional(traditional));
+	}
 
-    #[bench]
-    #[cfg(feature = "bench")]
-    fn bench_simplified_to_simplified(b: &mut Bencher) {
-        let simplified = "人人生而自由﹐在尊严和权利上一律平等。他们赋有理性和良心﹐并应以兄弟关系的精神互相对待。";
-        b.iter(|| traditional_to_simplified(simplified));
-    }
+	#[bench]
+	#[cfg(feature = "bench")]
+	fn bench_simplified_to_simplified(b: &mut Bencher) {
+		let simplified = "人人生而自由﹐在尊严和权利上一律平等。他们赋有理性和良心﹐并应以兄弟关系的精神互相对待。";
+		b.iter(|| traditional_to_simplified(simplified));
+	}
 
-    #[bench]
-    #[cfg(feature = "bench")]
-    fn bench_traditional_is_traditional(b: &mut Bencher) {
-        let traditional = "人人生而自由﹐在尊嚴和權利上一律平等。他們賦有理性和良心﹐並應以兄弟關係的精神互相對待。";
-        b.iter(|| is_traditional(traditional));
-    }
+	#[bench]
+	#[cfg(feature = "bench")]
+	fn bench_traditional_is_traditional(b: &mut Bencher) {
+		let traditional = "人人生而自由﹐在尊嚴和權利上一律平等。他們賦有理性和良心﹐並應以兄弟關係的精神互相對待。";
+		b.iter(|| is_traditional(traditional));
+	}
 
-    #[bench]
-    #[cfg(feature = "bench")]
-    fn bench_simplified_is_simplified(b: &mut Bencher) {
-        let simplified = "人人生而自由﹐在尊严和权利上一律平等。他们赋有理性和良心﹐并应以兄弟关系的精神互相对待。";
-        b.iter(|| is_simplified(simplified));
-    }
+	#[bench]
+	#[cfg(feature = "bench")]
+	fn bench_simplified_is_simplified(b: &mut Bencher) {
+		let simplified = "人人生而自由﹐在尊严和权利上一律平等。他们赋有理性和良心﹐并应以兄弟关系的精神互相对待。";
+		b.iter(|| is_simplified(simplified));
+	}
 
-    #[bench]
-    #[cfg(feature = "bench")]
-    fn bench_traditional_is_simplified(b: &mut Bencher) {
-        let traditional = "人人生而自由﹐在尊嚴和權利上一律平等。他們賦有理性和良心﹐並應以兄弟關係的精神互相對待。";
-        b.iter(|| !is_simplified(traditional));
-    }
+	#[bench]
+	#[cfg(feature = "bench")]
+	fn bench_traditional_is_simplified(b: &mut Bencher) {
+		let traditional = "人人生而自由﹐在尊嚴和權利上一律平等。他們賦有理性和良心﹐並應以兄弟關係的精神互相對待。";
+		b.iter(|| !is_simplified(traditional));
+	}
 
-    #[bench]
-    #[cfg(feature = "bench")]
-    fn bench_simplified_is_traditional(b: &mut Bencher) {
-        let simplified = "人人生而自由﹐在尊严和权利上一律平等。他们赋有理性和良心﹐并应以兄弟关系的精神互相对待。";
-        b.iter(|| !is_traditional(simplified));
-    }
+	#[bench]
+	#[cfg(feature = "bench")]
+	fn bench_simplified_is_traditional(b: &mut Bencher) {
+		let simplified = "人人生而自由﹐在尊严和权利上一律平等。他们赋有理性和良心﹐并应以兄弟关系的精神互相对待。";
+		b.iter(|| !is_traditional(simplified));
+	}
 }
